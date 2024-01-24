f56234b2000c1f086886644629e61e4b1f2015db
Max Rydahl Andersen <max@xam.dk>
2024-01-24 23:17:04 +0100

    fix parent ref. add gitignore and remove target content

--- deployment/target/generated-sources/annotations/com/codesimcoe/quarkus/fx/extension/deployment/QuarkusFxExtensionProcessor$$accessor.java
+++
@@ -1,8 +0,0 @@
-package com.codesimcoe.quarkus.fx.extension.deployment;
-@io.quarkus.Generated("Quarkus annotation processor")
-public final class QuarkusFxExtensionProcessor$$accessor {
-    private QuarkusFxExtensionProcessor$$accessor() {}
-    public static Object construct() {
-        return new QuarkusFxExtensionProcessor();
-    }
-}
